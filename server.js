c622b9747fa21bdcbc56c7194107eca8b7b7193f
Christine Trant <christinetrant@gmail.com>
2020-07-27 13:09:25 +0100

    adding beginnings of having the frontend and api talk to each other

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
-
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
 const app = express();
 // need the below to parse json:
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
+app.use(cors());
 // Until I get to learn databases, going to use a variable to store users for now:
 const database = {
 	users: [
@@ -52,7 +54,8 @@ app.post('/signin', (req, res) => {
     "password": "cookies"
 	}*/
 	if(req.body.email === database.users[0].email && req.body.password === database.users[0].password) {
-		res.json('Success')
+		// res.json('success')
+		res.json(database.users[0]);
 	} else {
 		res.status(400).json('error logging in');
 	}
@@ -71,12 +74,16 @@ app.post('/register', (req, res) => {
 	*/
 	// Using destructuring we grab (GET) what we need from req.body:
 	const { email, name, password } = req.body;
+	bcrypt.hash(password, null, null, function(err, hash) {
+		// Store hash in your password DB.
+	});
+
 	// We want to create a new user:
 	database.users.push({
 		id: '125',
 		name: name,
 		email: email,
-		password: password,
+		// password: password,
 		entries: 0,
 		joined: new Date()
 	})
@@ -119,6 +126,18 @@ app.put('/image', (req, res) => {
 		res.status(400).json('no such user');
 	}
 })
+
+
+// BCRYPT FOR HASHING PASSWORDS
+// Load hash from your password DB.
+// bcrypt.compare("bacon", hash, function(err, res) {
+//     // res == true
+// });
+// bcrypt.compare("veggies", hash, function(err, res) {
+//     // res = false
+// });
+
+
 // we can send app.listen a second parameter, which is a function within this function, it will run right after the 'listen' happens on port 3000.
 app.listen(3000, () => {
 	console.log('app is running on port 3000');
